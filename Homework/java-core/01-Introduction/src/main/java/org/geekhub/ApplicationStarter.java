5c193782448f8ec6672ae6ee24f8b274165d9276
Serhii Lotsman <lotsman.961@gmail.com>
2023-10-19 13:49:18 +0300

    L01 Edited ApplicationStarter.java: added message output "Hello World!"

--- Homework/java-core/01-Introduction/src/main/java/org/geekhub/ApplicationStarter.java
+++ Homework/java-core/01-Introduction/src/main/java/org/geekhub/ApplicationStarter.java
@@ -2,5 +2,7 @@ package org.geekhub;
 
 public class ApplicationStarter {
 
-    // TODO write code here AND REMOVE THIS MESSAGE
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
