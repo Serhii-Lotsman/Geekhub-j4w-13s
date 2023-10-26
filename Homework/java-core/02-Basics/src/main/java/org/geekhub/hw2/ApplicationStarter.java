ecd59fcef9e0ad5b9d068b9487b818c9ef591dff
Serhii Lotsman <lotsman.961@gmail.com>
2023-10-26 21:07:01 +0300

    Deleted useless exit() method; added array of names and marks; refactored output messages and added studentGradesInfo method; added getStudentAverageMark method

--- Homework/java-core/02-Basics/src/main/java/org/geekhub/hw2/ApplicationStarter.java
+++ Homework/java-core/02-Basics/src/main/java/org/geekhub/hw2/ApplicationStarter.java
@@ -5,35 +5,66 @@ import java.util.Scanner;
 public class ApplicationStarter {
 
     public static void main(String[] args) {
-        int numStudents = Integer.parseInt(args[0]);
+        int numOfStudents = Integer.parseInt(args[0]);
 
-        if (numStudents <= 0) {
-            exit();
+        if (numOfStudents <= 0) {
+            System.out.println("Please, check input arguments of the application");
+            System.exit(1);
         } else {
-            String[] names = new String[numStudents];
+            String[] names = new String[numOfStudents];
+            int numOfSubjects = 3;
+            int[][] marks = new int[numOfStudents][numOfSubjects];
 
             Scanner scanner = new Scanner(System.in);
 
-            for (int i = numStudents; i > 0; i--) {
-                System.out.printf("Enter information for student %s %n", numStudents - i + 1);
-                System.out.println("Student's Name: ");
-                String studentName = scanner.nextLine();
-                names[numStudents - i] = studentName;
+            for (int i = numOfStudents; i > 0; i--) {
+                System.out.printf("Enter information for student #%s %n", numOfStudents - i + 1);
+
+                System.out.print("Student's Name: ");
+                String studentName = scanner.next();
+                names[numOfStudents - i] = studentName;
+
+                System.out.print("Math Grade (0-100): ");
+                int mathMark = scanner.nextInt();
+                marks[numOfStudents - i][0] = mathMark;
+
+                System.out.print("Science Grade (0-100): ");
+                int scienceMark = scanner.nextInt();
+                marks[numOfStudents - i][1] = scienceMark;
+
+                System.out.print("History Grade (0-100): ");
+                int historyMark = scanner.nextInt();
+                marks[numOfStudents - i][2] = historyMark;
             }
             scanner.close();
-            studentInfo(names);
+            studentGradesInfo(names, getStudentAverageMark(marks));
         }
     }
 
-    public static void exit() {
-        System.out.println("Something went wrong.\n"
-            + "Please enter the correct number in the input parameters of the application");
+    public static void studentGradesInfo(String[] studentNames, double[] mark) {
+        int index = mark.length;
+        System.out.println("\n");
+        System.out.println("Average Grades for Each Student:");
+
+        for (String studentName : studentNames) {
+            System.out.print(studentName + ": " + mark[index - studentNames.length] + "\n");
+            index++;
+        }
     }
 
-    public static void studentInfo(String[] students) {
-        for (String student : students) {
-            System.out.println(student);
+    public static double[] getStudentAverageMark(int[][] allMarks) {
+        double[] averageMark = new double[allMarks.length];
+
+        for (int i = 0; i < allMarks.length; i++) {
+            int sum = 0;
+            int numSubjects = allMarks[i].length;
+
+            for (int j = 0; j < numSubjects; j++) {
+                sum += allMarks[i][j];
+            }
+            averageMark[i] = (double) sum / numSubjects;
         }
+        return averageMark;
     }
 
 }
