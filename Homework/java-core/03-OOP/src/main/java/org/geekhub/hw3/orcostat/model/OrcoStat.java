1122579048c12eee78f6e0be26e68a22110a4b08
Serhii Lotsman <lotsman.961@gmail.com>
2023-11-02 23:42:41 +0200

    L03 Created OrcoStat.java and implemented all methods Added import to OrcoStatTest.java All tests completed!

---
+++ Homework/java-core/03-OOP/src/main/java/org/geekhub/hw3/orcostat/model/OrcoStat.java
@@ -0,0 +1,30 @@
+package org.geekhub.hw3.orcostat.model;
+
+public class OrcoStat {
+    private int orcCounter;
+    private int dollarsCounter;
+
+    public void smashOrc(Orc orc) {
+        orc.scream();
+        orcCounter++;
+        dollarsCounter+=orc.getPrice();
+    }
+
+    public int getNegativelyAliveOrcCount() {
+        return orcCounter;
+    }
+
+    public void smashTechnique(Technique technique) {
+        technique.destroy();
+        dollarsCounter+=technique.getPrice();
+        if (technique.getEquipage().size() != 0) {
+            for (Object object : technique.getEquipage().getElements()) {
+                smashOrc((Orc) object);
+            }
+        }
+    }
+
+    public int getLosesInDollars() {
+        return dollarsCounter;
+    }
+}
