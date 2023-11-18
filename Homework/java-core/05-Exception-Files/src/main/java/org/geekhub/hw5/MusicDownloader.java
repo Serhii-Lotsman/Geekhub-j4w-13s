946572bbec9c630c7d7a882aeb76b242eca94fe6
Serhii Lotsman <lotsman.961@gmail.com>
2023-11-18 19:58:06 +0200

    L05 replaced hardcoded pathToPlaylist by correct relative path

--- Homework/java-core/05-Exception-Files/src/main/java/org/geekhub/hw5/MusicDownloader.java
+++ Homework/java-core/05-Exception-Files/src/main/java/org/geekhub/hw5/MusicDownloader.java
@@ -1,15 +1,11 @@
 package org.geekhub.hw5;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import static org.geekhub.hw5.FileUtils.deleteDirectories;
 
 public class MusicDownloader {
 
     public static void main(String[] args) {
-        Path pathToPlay = Paths.get("C:", "Programming", "Java", "GeekHub", "Homework", "java-core", "05-Exception-Files", "src", "main", "resources", "playlist.txt");
-        String pathToPlaylist = pathToPlay.toString();
+        String pathToPlaylist = "Homework/java-core/05-Exception-Files/src/main/resources/playlist.txt";
         String pathToLogFile = "log.txt";
         String mainDirectory = "library";
         int maxFileSize = 10485760;
