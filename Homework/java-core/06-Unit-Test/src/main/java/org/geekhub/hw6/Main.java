245794a813839892d7ac82b6f1e80ade251917fe
Serhii Lotsman <lotsman.961@gmail.com>
2023-11-23 21:46:55 +0200

    L06 added the implementation of fetching a path, creating a file and writing facts to it

--- Homework/java-core/06-Unit-Test/src/main/java/org/geekhub/hw6/Main.java
+++ Homework/java-core/06-Unit-Test/src/main/java/org/geekhub/hw6/Main.java
@@ -3,11 +3,25 @@ package org.geekhub.hw6;
 import com.google.gson.Gson;
 import org.apache.http.impl.client.HttpClientBuilder;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
 public class Main {
     public static void main(String[] args) throws CatFactException {
         var httpClient = HttpClientBuilder.create().build();
         var catFactService = new CatFactService(httpClient, new Gson());
 
-        System.out.println(catFactService.getRandomCatFact());
+        String fetchPath = System.getProperty("user.dir");
+        Path filePath = Path.of(fetchPath + Path.of("/resources/catFacts.txt"));
+        try {
+            if (!Files.exists(filePath)) {
+                Files.createFile(filePath);
+            }
+            Files.writeString(filePath, catFactService.getRandomCatFact() + "\n", StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
