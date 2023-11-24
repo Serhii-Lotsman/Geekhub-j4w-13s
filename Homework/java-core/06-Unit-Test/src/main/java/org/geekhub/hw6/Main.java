e28a707230bf61cbdc3e02a125b40210bd12c4fe
Serhii Lotsman <lotsman.961@gmail.com>
2023-11-24 22:20:55 +0200

    L06 added impl to fetch the facts; used contains method for duplicate

--- Homework/java-core/06-Unit-Test/src/main/java/org/geekhub/hw6/Main.java
+++ Homework/java-core/06-Unit-Test/src/main/java/org/geekhub/hw6/Main.java
@@ -12,16 +12,39 @@ public class Main {
     public static void main(String[] args) throws CatFactException {
         var httpClient = HttpClientBuilder.create().build();
         var catFactService = new CatFactService(httpClient, new Gson());
-
+        int retries = 5;
         String fetchPath = System.getProperty("user.dir");
         Path filePath = Path.of(fetchPath + Path.of("/resources/catFacts.txt"));
+
+        createFileIfNotExist(filePath);
+
         try {
-            if (!Files.exists(filePath)) {
-                Files.createFile(filePath);
+            while (retries != 0) {
+                if (Files.size(filePath) == 0) {
+                    Files.writeString(filePath, catFactService.getRandomCatFact() + "\n", StandardOpenOption.APPEND);
+                } else if (!Files.readAllLines(filePath).contains(catFactService.getRandomCatFact())) {
+                    Files.writeString(filePath, catFactService.getRandomCatFact() + "\n", StandardOpenOption.APPEND);
+                    retries = 5;
+                    Thread.sleep(1000);
+                } else if (catFactService.getRandomCatFact() == null){
+                    retries--;
+                } else retries--;
             }
-            Files.writeString(filePath, catFactService.getRandomCatFact() + "\n", StandardOpenOption.APPEND);
+            Files.writeString(filePath, "I don't know any new facts", StandardOpenOption.APPEND);
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        }
+    }
+    public static void createFileIfNotExist(Path filePath) {
+        if (!Files.exists(filePath)) {
+            try {
+                Files.createFile(filePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
