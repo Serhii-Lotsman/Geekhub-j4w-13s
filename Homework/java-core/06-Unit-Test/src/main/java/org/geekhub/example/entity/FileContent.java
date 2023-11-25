6041c4bbd5442c6f714319e1c335fffc9f56aa6c
Serhii Lotsman <lotsman.961@gmail.com>
2023-11-26 00:05:01 +0200

    L06 refactored code smell in Example package

--- Homework/java-core/06-Unit-Test/src/main/java/org/geekhub/example/entity/FileContent.java
+++ Homework/java-core/06-Unit-Test/src/main/java/org/geekhub/example/entity/FileContent.java
@@ -1,7 +1,23 @@
 package org.geekhub.example.entity;
 
-public record FileContent(
-    FileInfo fileInfo,
-    byte[] content
-) {
+import java.util.Arrays;
+import java.util.Objects;
+
+public record FileContent(FileInfo fileInfo, byte[] content) {
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(fileInfo);
+        result = 31 * result + Arrays.hashCode(content);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    }
 }
