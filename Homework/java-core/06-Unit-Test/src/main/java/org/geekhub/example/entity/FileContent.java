555973fb53f65c37208990e3814f2bd8b11eedca
Serhii Lotsman <lotsman.961@gmail.com>
2023-11-26 19:29:34 +0200

    L06 suppress warning in Example FileContent.java

--- Homework/java-core/06-Unit-Test/src/main/java/org/geekhub/example/entity/FileContent.java
+++ Homework/java-core/06-Unit-Test/src/main/java/org/geekhub/example/entity/FileContent.java
@@ -1,32 +1,8 @@
 package org.geekhub.example.entity;
 
-import java.util.Arrays;
-import java.util.Objects;
-
+@SuppressWarnings("java:S6218")
 public record FileContent(
     FileInfo fileInfo,
     byte[] content
 ) {
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        FileContent that = (FileContent) o;
-        return Objects.equals(fileInfo, that.fileInfo) && Arrays.equals(content, that.content);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Objects.hash(fileInfo);
-        result = 31 * result + Arrays.hashCode(content);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "FileContent{" +
-            "fileInfo=" + fileInfo +
-            ", content=" + Arrays.toString(content) +
-            '}';
-    }
 }
