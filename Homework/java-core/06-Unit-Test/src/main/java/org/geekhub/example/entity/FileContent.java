61cd746215f0da225a76d6d3089510303861eddf
Serhii Lotsman <lotsman.961@gmail.com>
2023-11-26 19:17:07 +0200

    Revert "L06 refactored code smell in Example package"
    This reverts commit 6041c4bbd5442c6f714319e1c335fffc9f56aa6c.

--- Homework/java-core/06-Unit-Test/src/main/java/org/geekhub/example/entity/FileContent.java
+++ Homework/java-core/06-Unit-Test/src/main/java/org/geekhub/example/entity/FileContent.java
@@ -1,23 +1,7 @@
 package org.geekhub.example.entity;
 
-import java.util.Arrays;
-import java.util.Objects;
-
-public record FileContent(FileInfo fileInfo, byte[] content) {
-    @Override
-    public int hashCode() {
-        int result = Objects.hash(fileInfo);
-        result = 31 * result + Arrays.hashCode(content);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "";
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return false;
-    }
+public record FileContent(
+    FileInfo fileInfo,
+    byte[] content
+) {
 }
