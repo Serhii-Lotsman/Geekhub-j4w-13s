16e3e6ed608f075ef584fb315537401aff048d50
Serhii Lotsman <lotsman.961@gmail.com>
2024-01-20 23:48:35 +0200

    L12 added only inputSpec path

--- Homework/java-core/12-Gradle/open-api-generation/build.gradle.kts
+++ Homework/java-core/12-Gradle/open-api-generation/build.gradle.kts
@@ -4,25 +4,15 @@ plugins {
 
 buildscript {
     repositories {
-        mavenLocal()
         mavenCentral()
     }
-    dependencies {
-        classpath("org.openapitools:openapi-generator-gradle-plugin:7.2.0")
-    }
 }
 
-apply(plugin = ("org.openapi.generator"))
 
-tasks.register("openAPI-starter") {
-    openApiGenerate {
-        group = "custom-OpenAPI"
-        generatorName.set("kotlinOpenAPI")
-        inputSpec.set("src/main/resources/openapi.yaml")
-        outputDir.set("build/openapi-generated")
-        modelPackage.set("build/openapi-generated/model")
-        generateApiDocumentation.set(false)
-        generateApiTests.set(false)
-    }
-
+openApiGenerate {
+    generatorName.set("java")
+    inputSpec.set("$projectDir/src/main/resources/openapi.yaml")
+    generateApiTests.set(false)
+    generateApiDocumentation.set(false)
 }
+
