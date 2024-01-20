39925874e01b8884b1800bcc59057d266d628b6d
Serhii Lotsman <lotsman.961@gmail.com>
2024-01-20 17:11:44 +0200

    L12 modified apache dependency to avoid problems with task 6

--- Homework/java-core/06-Unit-Test/build.gradle.kts
+++ Homework/java-core/06-Unit-Test/build.gradle.kts
@@ -1,5 +1,8 @@
 dependencies {
-    implementation("org.apache.httpcomponents.client5:httpclient5:5.0")
+    implementation("org.apache.httpcomponents:httpclient:4.5.13") {
+        exclude(group = "commons-codec", module = "commons-codec")
+    }
+    implementation("commons-codec:commons-codec:1.13")
     implementation ("com.google.code.gson:gson:2.10.1")
 
     testImplementation (platform("org.junit:junit-bom:5.9.1"))
