7a31ea87f4d22c4482eb605375c027ac6d857eb3
Serhii Lotsman <lotsman.961@gmail.com>
2024-02-10 21:06:41 +0200

    L15 edited build.gradle.kts

--- Homework/java-web/cipher/build.gradle.kts
+++ Homework/java-web/cipher/build.gradle.kts
@@ -4,8 +4,16 @@ plugins {
 }
 
 repositories {
-    mavenCentral()
+    maven(url = "https://repsy.io/mvn/vrudas/slotsman-j4w-s13-repo") {
+        credentials {
+            username = System.getenv("REPSY_LOGIN")
+            password = System.getenv("REPSY_PASSWORD")
+        }
+    }
 }
+
+group = "com.ciphers"
+version = "1.0.0"
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
@@ -16,39 +24,27 @@ tasks.test {
     useJUnitPlatform()
 }
 
-val mainSourceSet = sourceSets.getByName("main")
-
-val sourceJar = tasks.register<Jar>("cipher") {
-    from(mainSourceSet.java)
+val publishToRepsy by tasks.registering {
+    dependsOn("publish")
+    doLast {
+        println("Publishing to repsy.io Maven repository...")
+    }
 }
 
 publishing {
-    repositories {
-        maven {
-            name = "CipherStorage"
-            url = uri("https://repsy.io/mvn/vrudas/slotsman-j4w-s13-repo")
-            credentials {
-                username = project.findProperty("REPSY_USER")?.toString() ?: System.getenv("REPSY_USER")
-                password = project.findProperty("REPSY_PASSWORD")?.toString() ?: System.getenv("REPSY_PASSWORD")
-            }
+    publications {
+        create<MavenPublication>("Cipher") {
+            from(components["java"])
         }
     }
-    publications {
-        create<MavenPublication>("CipherPublication") {
-            from(components["java"])
-            artifact(sourceJar)
-            groupId = "com.ciphers"
-            version = "1.0.0"
-            artifactId = "cipherAlgorithms"
-            description = "A library for cryptographic algorithms."
-            pom {
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
+    repositories {
+        maven {
+            url = uri("https://repsy.io/mvn/vrudas/slotsman-j4w-s13-repo")
+            credentials {
+                username = System.getenv("REPSY_LOGIN")
+                password = System.getenv("REPSY_PASSWORD")
             }
         }
     }
 }
+
