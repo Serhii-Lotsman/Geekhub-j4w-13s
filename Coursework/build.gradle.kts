bb7da422a3154f072e8c05bca4124b9a6a65598b
Serhii Lotsman <lotsman.961@gmail.com>
2024-02-25 17:48:17 +0200

    CW added .env.sample with credentials; created CrewCraftStarter.java with DatabaseConfig.java; added docker-compose.yaml; created application.properties with default and prod profiles

---
+++ Coursework/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "org.geekhub.crew-craft"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:3.0.0")
+    implementation("org.postgresql:postgresql:42.7.2")
+    implementation("com.jayway.jsonpath:json-path:2.9.0")
+    implementation("org.flywaydb:flyway-core:10.8.1")
+    runtimeOnly("org.flywaydb:flyway-database-postgresql:10.8.1")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.2.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ Coursework/src/main/java/org/geekhub/crewcraft/CrewCraftStarter.java
@@ -0,0 +1,11 @@
+package org.geekhub.crewcraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewCraftStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(CrewCraftStarter.class, args);
+    }
+}
