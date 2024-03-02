72e11793141c7c7692f5df8b0bd4752d8621eee6
Serhii Lotsman <lotsman.961@gmail.com>
2024-03-02 15:43:09 +0200

    Edited build.gradle.kts

--- Homework/cipher/build.gradle.kts
+++ Homework/cipher/build.gradle.kts
@@ -36,8 +36,8 @@ publishing {
         maven {
             url = uri("https://repsy.io/mvn/slotsman/cipher-storage-repository")
             credentials {
-                username = System.getenv("REPSY_LOGIN") ?: providers.gradleProperty("username").get()
-                password = System.getenv("REPSY_PASSWORD") ?: providers.gradleProperty("password").get()
+                username = System.getenv("REPSY_LOGIN")
+                password = System.getenv("REPSY_PASSWORD")
             }
         }
     }
