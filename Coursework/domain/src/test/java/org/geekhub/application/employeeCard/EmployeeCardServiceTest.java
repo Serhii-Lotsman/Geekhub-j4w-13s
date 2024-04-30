92813f2827b7fb5011ba491406f3e663d9db402d
Serhii Lotsman <lotsman.961@gmail.com>
2024-04-30 17:32:43 +0300

    CW added CustomUserDetailsServiceTest.java, EmployeeCardServiceTest.java

---
+++ Coursework/domain/src/test/java/org/geekhub/application/employeeCard/EmployeeCardServiceTest.java
@@ -0,0 +1,164 @@
+package org.geekhub.application.employeeCard;
+
+import org.geekhub.application.employeeCard.model.EmployeeCardEntity;
+import org.geekhub.application.enums.EmployeeGender;
+import org.geekhub.application.enums.EmployeePosition;
+import org.geekhub.application.exception.EmployeeCardException;
+import org.geekhub.application.exception.UserException;
+import org.geekhub.application.exception.UserExistException;
+import org.geekhub.application.user.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class EmployeeCardServiceTest {
+
+    public static final long EMPLOYEE_ID = 1L;
+    @Mock
+    private EmployeeCardRepository employeeCardRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    private EmployeeCardService employeeCardService;
+
+    private final EmployeeCardEntity employeeCardEntity = new EmployeeCardEntity(
+        "Firstname",
+        "Lastname",
+        LocalDate.of(2001,1, 1),
+        "test@gmail.com",
+        EmployeePosition.ACCOUNTANT,
+        "City",
+        false,
+        EmployeeGender.FEMALE,
+        LocalDate.of(2024,1, 1)
+    );
+
+    @BeforeEach
+    public void setUp() {
+        employeeCardService = new EmployeeCardService(employeeCardRepository, userRepository);
+    }
+
+    @Test
+    public void testSaveEmployeeCard_shouldSaveCard_whenValidEmployee() {
+        when(employeeCardRepository.employeeEmailExist("test@gmail.com")).thenReturn(false);
+        when(userRepository.existsByUserEmail("test@gmail.com")).thenReturn(true);
+
+        assertDoesNotThrow(() -> employeeCardService.saveEmployeeCard(employeeCardEntity));
+
+        verify(employeeCardRepository, times(1)).saveEmployeeCard(employeeCardEntity);
+    }
+
+    @Test
+    public void testSaveEmployeeCard_shouldThrowUserExistException_whenEmailExists() {
+        when(employeeCardRepository.employeeEmailExist("test@gmail.com")).thenReturn(true);
+
+        assertThrows(UserExistException.class, () -> employeeCardService.saveEmployeeCard(employeeCardEntity));
+        verify(employeeCardRepository, never()).saveEmployeeCard(any());
+    }
+
+    @Test
+    public void testSaveEmployeeCard_shouldThrowUserException_whenEmailNotFoundInUserRepository() {
+        employeeCardEntity.setEmail("nonExistent@gmail.com");
+        when(employeeCardRepository.employeeEmailExist("nonExistent@gmail.com")).thenReturn(false);
+        when(userRepository.existsByUserEmail("nonExistent@gmail.com")).thenReturn(false);
+
+        assertThrows(UserException.class, () -> employeeCardService.saveEmployeeCard(employeeCardEntity));
+        verify(employeeCardRepository, never()).saveEmployeeCard(any());
+    }
+
+    @Test
+    public void testUpdateEmployeeCard_shouldUpdateEmployeeCard_whenValidEmployee() {
+        EmployeeCardEntity updatedEmployeeCardEntity = this.employeeCardEntity;
+        updatedEmployeeCardEntity.setFirstName("Updated");
+        updatedEmployeeCardEntity.setLastName("Updated");
+        assertDoesNotThrow(() -> employeeCardService.updateEmployeeCard(updatedEmployeeCardEntity));
+
+        verify(employeeCardRepository, times(1)).updateEmployeeCard(employeeCardEntity);
+    }
+
+    @Test
+    public void testDeleteEmployeeCard_shouldDeleteCard_whenValidId() {
+        when(employeeCardRepository.getEmployeeCard(EMPLOYEE_ID))
+            .thenReturn(Optional.of(new EmployeeCardEntity()));
+
+        assertDoesNotThrow(() -> employeeCardService.deleteEmployeeCard(EMPLOYEE_ID));
+        verify(employeeCardRepository, times(1)).deleteEmployeeCard(EMPLOYEE_ID);
+    }
+
+    @Test
+    public void testDeleteEmployeeCard_shouldThrowEmployeeCardException_whenInvalidId() {
+        Long employeeId = -EMPLOYEE_ID;
+        when(employeeCardRepository.getEmployeeCard(employeeId)).thenReturn(Optional.empty());
+
+        assertThrows(EmployeeCardException.class,
+            () -> employeeCardService.deleteEmployeeCard(employeeId));
+        verify(employeeCardRepository, never()).deleteEmployeeCard(employeeId);
+    }
+
+    @Test
+    public void testGetEmployeeCardById_shouldReturnEmployeeCard_whenValidId() {
+        EmployeeCardEntity employeeCardEntity = new EmployeeCardEntity();
+        when(employeeCardRepository.getEmployeeCard(EMPLOYEE_ID))
+            .thenReturn(Optional.of(employeeCardEntity));
+
+        EmployeeCardEntity result = employeeCardService.getEmployeeCardById(EMPLOYEE_ID);
+        assertEquals(employeeCardEntity, result);
+    }
+
+    @Test
+    public void testGetEmployeeCardById_shouldThrowEmployeeCardException_whenInvalidId() {
+        Long employeeId = -EMPLOYEE_ID;
+        when(employeeCardRepository.getEmployeeCard(employeeId)).thenReturn(Optional.empty());
+
+        assertThrows(EmployeeCardException.class,
+            () -> employeeCardService.getEmployeeCardById(employeeId));
+    }
+
+    @Test
+    public void testGetEmployeeCardByEmail_shouldReturnEmployeeCardByEmail_whenValidEmail() {
+        String email = "test@gmail.com";
+        EmployeeCardEntity employeeCardEntity = new EmployeeCardEntity();
+        when(employeeCardRepository.getEmployeeCard(email)).thenReturn(Optional.of(employeeCardEntity));
+
+        EmployeeCardEntity result = employeeCardService.getEmployeeCardByEmail(email);
+        assertEquals(employeeCardEntity, result);
+    }
+
+    @Test
+    public void testGetEmployeeCardByEmail_shouldThrowEmployeeCardException_whenInvalidEmail() {
+        String email = "nonExistent@gmail.com";
+        when(employeeCardRepository.getEmployeeCard(email)).thenReturn(Optional.empty());
+
+        assertThrows(EmployeeCardException.class,
+            () -> employeeCardService.getEmployeeCardByEmail(email));
+    }
+
+    @Test
+    public void testGetAllEmployees_shouldReturnListOfEmployeeCard_always() {
+        int pageNum = 1;
+        int pageSize = 10;
+        List<EmployeeCardEntity> employeeList = Collections.emptyList();
+        when(employeeCardRepository.getEmployeeCards(pageNum, pageSize)).thenReturn(employeeList);
+
+        List<EmployeeCardEntity> result = employeeCardService.getAllEmployees(pageNum, pageSize);
+        assertEquals(employeeList, result);
+    }
+}
---
+++ Coursework/domain/src/test/java/org/geekhub/application/user/CustomUserDetailsServiceTest.java
@@ -0,0 +1,121 @@
+package org.geekhub.application.user;
+
+import org.geekhub.application.exception.UserException;
+import org.geekhub.application.exception.UserExistException;
+import org.geekhub.application.exception.ValidationException;
+import org.geekhub.application.user.model.UserEntity;
+import org.geekhub.application.user.model.UserRole;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CustomUserDetailsServiceTest {
+
+    private static final int USER_ID = 1;
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private UserRoleRepository userRoleRepository;
+
+    private CustomUserDetailsService customUserDetailsService;
+
+    @BeforeEach
+    public void setUp() {
+        customUserDetailsService = new CustomUserDetailsService(userRepository, userRoleRepository);
+    }
+
+    @Test
+    public void testCreateUser_shouldReturnId_whenValidUser() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setEmail("test@gmail.com");
+        userEntity.setPassword("password");
+
+        when(userRepository.saveUser(userEntity)).thenReturn(USER_ID);
+        int userId = customUserDetailsService.createUser(userEntity);
+
+        verify(userRepository, times(1)).saveUser(userEntity);
+        assertEquals(USER_ID, userId);
+    }
+
+    @Test
+    public void testCreateUser_shouldThrowValidationException_whenInvalidEmail() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setEmail("invalid_email");
+        userEntity.setPassword("password");
+
+        assertThrows(ValidationException.class, () -> customUserDetailsService.createUser(userEntity));
+        verify(userRepository, never()).saveUser(any());
+    }
+
+    @Test
+    public void testCreateUser_shouldThrowUserExistException_whenEmailExists() {
+        UserEntity existingUser = new UserEntity();
+        existingUser.setEmail("existing@gmail.com");
+        existingUser.setPassword("password");
+
+        when(userRepository.existsByUserEmail("existing@gmail.com")).thenReturn(true);
+        assertThrows(UserExistException.class, () -> customUserDetailsService.createUser(existingUser));
+        verify(userRepository, never()).saveUser(any());
+    }
+
+    @Test
+    public void testIsEmailExist_shouldReturnTrue_whenEmailExists() {
+        when(userRepository.existsByUserEmail("existing@gmail.com")).thenReturn(true);
+        assertTrue(customUserDetailsService.isEmailExist("existing@gmail.com"));
+    }
+
+    @Test
+    public void testIsEmailExist_shouldReturnFalse_whenEmailDoesNotExist() {
+        when(userRepository.existsByUserEmail("nonExistent@gmail.com")).thenReturn(false);
+        assertFalse(customUserDetailsService.isEmailExist("nonExistent@gmail.com"));
+    }
+
+    @Test
+    public void testSetUserRole_shouldSetRole_always() {
+        doNothing().when(userRoleRepository).assignRole(anyInt(), anyInt());
+
+        customUserDetailsService.setUserRole(USER_ID, 3);
+        verify(userRoleRepository, times(1)).assignRole(USER_ID, 3);
+    }
+
+    @Test
+    public void testGetRoleByName_shouldReturnAssignedRole_whenRoleExists() {
+        String roleName = "USER";
+        UserRole userRole = new UserRole(USER_ID, roleName);
+
+        when(userRoleRepository.findRole(roleName)).thenReturn(Optional.of(userRole));
+        UserRole resultRole = customUserDetailsService.getRoleByName(roleName);
+        assertEquals(userRole, resultRole);
+    }
+
+    @Test
+    public void testGetRoleByName_shouldThrowUserException_whenRoleNotFound() {
+        String roleName = "NON_EXISTING_ROLE";
+
+        when(userRoleRepository.findRole(roleName)).thenReturn(Optional.empty());
+        assertThrows(UserException.class, () -> customUserDetailsService.getRoleByName(roleName));
+    }
+
+    @Test
+    public void testLoadUserByUsername_shouldThrowUsernameNotFoundException_whenUserNotFound() {
+        when(userRepository.findUserByEmail("nonExistent@gmail.com")).thenReturn(Optional.empty());
+        assertThrows(UsernameNotFoundException.class,
+            () -> customUserDetailsService.loadUserByUsername("nonExistent@gmail.com"));
+    }
+}
