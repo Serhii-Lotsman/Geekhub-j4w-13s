c28c2e082a5dfaf885b2812473079619446478cf
Serhii Lotsman <lotsman.961@gmail.com>
2024-06-01 16:19:58 +0300

    CW delete unused SecurityConstant.java

--- Coursework/application/src/main/java/org/geekhub/application/config/SecurityConstant.java
+++
@@ -1,6 +0,0 @@
-package org.geekhub.application.config;
-
-public class SecurityConstant {
-    public static final long JWT_EXPIRATION = 70000;
-    public static final String JWT_SECRET = "secret";
-}
